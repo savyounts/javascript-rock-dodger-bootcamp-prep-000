b54a8ed4a54d4cb1597f8da43d6b1e0058b5cca1
Sav Younts <savyounts@gmail.com>
2018-05-31 00:26:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -154,7 +154,11 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-   
+   var leftNumbers = dodger.style.left.replace('px', '')
+   var left = parseInt(leftNumbers, 10)
+   if (left>0){
+     dodger.style.left = `${left += 4}px`
+   }
 }
 
 function moveDodgerRight() {
