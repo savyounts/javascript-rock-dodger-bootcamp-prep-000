3226abf2e7c6e3f1dddba1aec30f7d2b31b8bde0
Sav Younts <savyounts@gmail.com>
2018-05-31 00:22:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -141,10 +141,10 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-   if (e.which === 37){
+   if (e.which === LEFT_ARROW){
      moveDodgerLeft()
    }
-   if (e.which === 39){
+   if (e.which === RIGHT_ARROW){
      moveDodgerRight()
    }
 }
